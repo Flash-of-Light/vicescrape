5c2bd5a3e0ee2476420796b916432382878b26a9
Jay Goss <jayspgoss@gmail.com>
2019-01-16 23:26:00 -0500

    scraping

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3002;
 var app = express();
 
 app.use(logger("dev"));
@@ -14,53 +14,55 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
-app.get("assets/vicelogo.png", function (req, res) {
-    res.sendFile(path.join(__dirname, "assets/vicelogo.png"));
- });
-
+//route for scraping articles
 app.get("/scrape", function(req, res) {
   axios.get("http://www.vice.com/en_us").then(function(response) {
     var $ = cheerio.load(response.data);
-    // console.log($("div.grid__wrapper grd-row").children("a"));
-    // Now, we grab every h2 within an article tag, and do the following:
     $("div.grid__wrapper").children("a").each(function(i, element) {
-        console.log("For each function");
-// console.log(i);
-console.log($(element).text());
-console.log($(element).attr("href"));
-    //   var result = {};
-    //   result.title = $(this)
-    //     .children("a")
-    //     .text();
-    //   result.link = $(this)
-    //     .children("a")
-    //     .attr("href");
+    console.log($(element).text());
+    console.log($(element).attr("href"));
 
-      // Create a new Article using the `result` object built from scraping
-    //   db.article.create(result)
-    //     .then(function(dbarticle) {
-    //       // View the added result in the console
-    //       console.log(dbarticle);
-    //     })
-    //     .catch(function(err) {
-    //       // If an error occurred, log it
-    //       console.log(err);
-    //     });
+    var result = {};
+
+    result.title = $(element).text();
+    result.link = $(element).attr("href");
+    db.article.create(result)
+      .then(function(dbarticle) {
+        console.log(dbarticle);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
     });
 
-    // Send a message to the client
-    res.send("Scrape Complete");
+  res.send("Scrape Complete");
   }).catch(err => {
-      console.log(err);
+    console.log(err);
   });
 });
 
-// Route for getting all Articles from the db
+// Route for showing saved articles
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
   db.article.find({})
     .then(function(dbarticle) {
       // If we were able to successfully find Articles, send them back to the client
+      res.json(dbarticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbarticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbarticle);
     })
     .catch(function(err) {
